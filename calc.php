e55f44311401595643fb88a28f47ce1723417a9d
G. Logan Slinski <phantom.patrol@gmail.com>
2009-08-11 19:29:48 +0000

    woot
    git-svn-id: svn+ssh://dev/usr/local/svn/rentalcar@22 4e2ffa78-7f97-452c-ab07-10394248896c

--- calc.php
+++ calc.php
@@ -1,3 +1,117 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+
+<html>
+  <head>
+    <title>Vehicle Rental Service</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <link href="stylesheets/base.css" rel="stylesheet" type="text/css">
+<script src="javascript/prototype.js"></script>
+<script type="text/javascript">
+var typedSoFar = new String;
+function konami(e) {
+  //66 is keycode for B, 65 is keycode for A
+  var konami = Event.KEY_UP + "" + Event.KEY_UP + "" + Event.KEY_DOWN + "" + Event.KEY_DOWN;
+  konami += Event.KEY_LEFT + "" + Event.KEY_RIGHT + "" + Event.KEY_LEFT + "" + Event.KEY_RIGHT;
+  konami += '6665';
+  typedSoFar += e.keyCode;
+  if(typedSoFar.include(konami)){
+    window.location = "http://www.yougotrickrolled.com/";
+  }
+}
+
+</script>
+  </head>
+  
+  <body onkeydown="konami(event)">
+    <div id="container" >
+      <!-- Begin UMass Amherst top banner --> 
+      <div id="topbanner"> 
+        <a  href="http://umass.edu">
+          <img id="banner_wordmark" src="images/informal_fff_on_000.gif" alt="UMass Amherst">
+        </a>
+          <form id="banner_search" action="http://googlebox.oit.umass.edu/search" method="get" name="gs" onsubmit="if (this.q.value=='Search UMass Amherst') return false;">
+            <div>
+              <label for="q">
+              <input name="q" id="q" size="22" value="Search UMass Amherst" onfocus="if (this.value=='Search UMass Amherst') this.value=''" onblur="if (this.value=='') this.value='Search UMass Amherst'" type="text">
+              </label>
+              <input input="submit" name="sa" value="Go" type="submit">
+              <input name="site" value="default_collection" type="hidden">
+              <input name="client" value="default_frontend" type="hidden">
+              <input name="proxystylesheet" value="default_frontend" type="hidden">
+              <input name="output" value="xml_no_dtd" type="hidden">
+            </div>
+          </form>
+      </div>
+      
+      <div id="banner">
+        <!--Banner image rendered through css-->
+        <a id="banner_link" href="index.html"></a>
+      </div>
+      
+      <div id="topmenu"> 
+        <a href="index.html">HOME</a>
+        <a href="http://www.umass.edu/transit/buses.html">PVTA BUSES</a>
+        <a href="http://www.umass.edu/transit/fieldtrip.html">FIELD TRIP</a>
+        <a href="http://www.umass.edu/transit/meet_greet.html">MEET &amp; GREET</a>
+        <a href="contactus.html">CONTACT US</a>
+      </div>
+      
+      <table id="main_table">
+        <tbody>
+          <tr>
+             <td id="left">
+                
+                
+
+                
+                <div class="left_menu_title">
+                  Rental Information
+                </div>
+                <div id="left_services">
+                
+                  <a href="pdf/UMass_Car_Class_with_Rates_Flyer.pdf">Car Class Guide</a><br>
+
+                  <a href="compare.html">Comparison / Calculator</a><br>
+
+						      <a href="localoffice.html">Local Contact</a><br>
+
+                  <a href="https://people.umass.edu/transit/rentalcar">Reservations & Rates</a><br>
+                
+                </div>
+                
+
+
+
+                <div class="left_menu_title">
+                  UMass Travel Resources
+                </div>
+                <div id="left_enterprise_links"> 
+                	
+									<a href="http://www.umass.edu/travel/car.html">Auto Insurance</a><br><br>
+
+
+                  <a href="http://www.umass.edu/travel/corpCard.html">Corporate Travel Card Program</a><br><br>
+
+							
+							    <a href="http://www.umass.edu/travel/files/mileage_rate.pdf">Mileage Reimbursement Rate</a><br><br>
+            
+
+                  <a href="http://www.umass.edu/travel/agencies.html">Travel Agency</a><br><br>
+								
+                </div>
+                
+                             
+             </td>
+
+
+            <!-- InstanceBeginEditable name="Content" -->
+            <td id="right">
+              <div id="page_title">Rental Cost Calculator</div>
+              <div id="page_text_field">
+
+
 <?
 //print "Damn it feels good to be a gangstar <br>";
 
@@ -79,3 +193,29 @@ print "Alternatively, it will cost your department $$reim_final_cost to use a pe
 
 
 ?>
+
+              </div>
+            </td>   
+          </tr>
+        </tbody>
+      </table>
+
+      <table id="footer_table">
+        <tbody>
+          <tr>
+            <td>
+              <div id="footer">
+                <p><br>
+                  This page is maintained by <a href="index.html">UMass Transit Services</a>.<br>
+                  &copy; 2009 <a href="http://www.umass.edu/">University of Massachusetts Amherst</a> &#8226; <a href="http://umass.edu/umhome/policies.html">Site Policies</a><br>
+                  <br>
+                  <img src="images/seal.jpg" id="seal" alt="Seal"></p>
+              </div>
+            </td>
+          </tr>
+      </tbody>
+    </table>
+  </div>
+  </body>
+</html>
+
