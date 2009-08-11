ddae3e7f9839d14789dd5f9a665cd5c36c75a40a
G. Logan Slinski <phantom.patrol@gmail.com>
2009-08-11 18:05:50 +0000

    ahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absfahsdfbvlkhbdflvkhbalbv;kabfvk;absf
    git-svn-id: svn+ssh://dev/usr/local/svn/rentalcar@16 4e2ffa78-7f97-452c-ab07-10394248896c

--- calc.php
+++ calc.php
@@ -1,3 +1,16 @@
 <?
-print "Damn it feels good to be a gangstar";
+print "Damn it feels good to be a gangstar <br>";
+
+$vehicle_type = $_POST['vehicle_type'];
+$days_in_trip = $_POST['days_in_trip'];
+$destination = $_POST['dest'];
+$use_round_trip = $_POST['use_distance_instead'];
+$round_trip_distance = $_POST['distance'];
+$cost_of_fuel = $_POST['cost_of_fuel'];
+$reimbursment_rate = $_POST['reim_rate'];
+
+print $vehicle_type;
+
+
+
 ?>
