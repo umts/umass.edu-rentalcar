b7e75555c182af4c186e6fbeece33c5853fcfb19
G. Logan Slinski <phantom.patrol@gmail.com>
2009-08-11 18:23:07 +0000

    more shit for the calculator
    git-svn-id: svn+ssh://dev/usr/local/svn/rentalcar@17 4e2ffa78-7f97-452c-ab07-10394248896c

--- calc.php
+++ calc.php
@@ -8,8 +8,13 @@ $use_round_trip = $_POST['use_distance_instead'];
 $round_trip_distance = $_POST['distance'];
 $cost_of_fuel = $_POST['cost_of_fuel'];
 $reimbursment_rate = $_POST['reim_rate'];
+$chosen_vehicle_rating;
+$final_cost;
 
-print $vehicle_type;
+
+print "Renting a $vehicle_type type vehicle, at $$cost_of_fuel a gallon, for $days_in_trip day(s), making a $round_trip_distance mile trip will cost your department approximately TONS OF MONIES! <br> <br>";
+
+print "Alternatively, it will cost MORE TONS OF MONIES!";
 
 
 
