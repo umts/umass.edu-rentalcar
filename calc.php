807f8529a2c4becfa91adf2857aee94a6f5bf359
G. Logan Slinski <phantom.patrol@gmail.com>
2009-08-11 18:49:24 +0000

    old school testing, the old school way
    git-svn-id: svn+ssh://dev/usr/local/svn/rentalcar@18 4e2ffa78-7f97-452c-ab07-10394248896c

--- calc.php
+++ calc.php
@@ -1,5 +1,5 @@
 <?
-print "Damn it feels good to be a gangstar <br>";
+//print "Damn it feels good to be a gangstar <br>";
 
 $vehicle_type = $_POST['vehicle_type'];
 $days_in_trip = $_POST['days_in_trip'];
@@ -8,10 +8,62 @@ $use_round_trip = $_POST['use_distance_instead'];
 $round_trip_distance = $_POST['distance'];
 $cost_of_fuel = $_POST['cost_of_fuel'];
 $reimbursment_rate = $_POST['reim_rate'];
-$chosen_vehicle_rating;
-$final_cost;
+$chosen_vehicle_rate = '0';
+$chosen_vehicle_fuel_rating = '0';
+$fuel_used = '0';
+$final_cost = '0';
 
 
+//Assign Values to the Vehicle rental cost & the MPG rating of the car
+switch($_POST['vehicle_type'])
+  {
+    case 'Compact':
+      $chosen_vehicle_rate = '29.99';
+      $chosen_vehicle_fuel_rating = '30';
+      break;
+
+    case 'Intermediate':
+      break;
+
+    case 'Standard':
+      break;
+    
+    case 'Full Size':
+      $chosen_vehicle_rate = '40.99';
+      $chosen_vehicle_fuel_rating = '22';
+      break;
+    
+    case 'Premium':
+      break;
+    
+    case 'Pickup Truck':
+      break;
+    
+    case 'Luxury':
+      break;
+    
+    case 'Minivan':
+      break;
+    
+    case 'SUV':
+      $chosen_vehicle_rate = '59.99';
+      $chosen_vehicle_fuel_rating = '19';
+      break;
+    
+    case '12 Pax Van':
+      break;
+    
+    case 'Cargo Van':
+      break;
+
+  }
+
+//fucking doing the math (please do not get on my case about the lack of functions, this was just thrown together)
+
+
+
+
+//crappy print statements
 print "Renting a $vehicle_type type vehicle, at $$cost_of_fuel a gallon, for $days_in_trip day(s), making a $round_trip_distance mile trip will cost your department approximately TONS OF MONIES! <br> <br>";
 
 print "Alternatively, it will cost MORE TONS OF MONIES!";
